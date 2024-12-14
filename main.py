c0b2bc276802b98d3304d8f3f28b2ba8d2341382
praveen23124 <pkandakatla44@gmail.com>
2024-12-14 18:52:07 +0530

    snake game part 2

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-from turtle import Screen, Turtle
+from turtle import Screen
 from snake import Snake
+from food import Food
+from scoreboard import Scoreboard
 import time
+
 screen = Screen()
 screen.setup(width = 80, height=680)
 screen.bgcolor("black")
@@ -8,6 +11,9 @@ screen.title("My snake Game")
 screen.tracer(0)
 
 snake = Snake()
+scoreboard = Scoreboard()
+food = Food()
+
 
 screen.listen()
 screen.onkey(snake.up,"Up")
@@ -20,39 +26,22 @@ while game_is_on:
     screen.update()
     time.sleep(0.1)
     snake.move()
-# segments = []
-#
-# for position in starting_positions:
-#     new_segment = Turtle("square")
-#     new_segment.color("white")
-#     new_segment.penup()
-#     new_segment.goto(position)
-#     segments.append(new_segment)
 
+    #detect collision with food
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        scoreboard.add()
+        snake.extend()
+   #detect collision with wall
+    if snake.head.xcor()>280 or snake.head.xcor()<-280 or snake.head.ycor()>280 or snake.head.ycor()<-280:
+        game_is_on = False
+        scoreboard.game_over()
 
-# game_is_on = True
-# while game_is_on:
-#     screen.update()
-#     time.sleep(0.1)
-#     for seg_num in range(len(segments)-1,0, -1):
-#         new_x = segments[seg_num - 1].xcor()
-#         new_y = segments[seg_num - 1].ycor()
-#         segments[seg_num].goto(new_x,new_y)
-#
-#     segments[0].forward(20)
-
-
-
-
-
-
-
-
-
-
-
-
-
+    #detect collision with tail
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment)<10:
+            game_is_on = False
+            scoreboard.game_over()
 
 
 
